54378b96878193033becf074900c88e51f62bfac
cibergarri <cibergarri@hotmail.com>
2021-05-19 01:08:21 +0200

    fix(contacts): Issue #11  - require correct file to export Contacts

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Reports = require('./src/Reports');
 const Journeys = require('./src/Journeys');
-const Contacts = require('./src/Campaigns');
+const Contacts = require('./src/Contacts');
 const Campaigns = require('./src/Campaigns');
 const AbTestCampaigns = require('./src/AbTestCampaigns');
 const ProductAndRevenue = require('./src/ProductAndRevenue');
